1fbd0819844459c4731ea900f47a6e83fd81eb53
Esraa Aldeeb <edeeb@ccecentrix.com>
2016-02-28 10:21:25 +0200

    Initial Commit

---
+++ NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by Esraa Aldeeb on 2/28/16.
+//  Copyright © 2016 Esraa Aldeeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProject
+//
+//  Created by Esraa Aldeeb on 2/28/16.
+//  Copyright © 2016 Esraa Aldeeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
